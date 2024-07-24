45156022f1dde0ea171e851404c208a0299d23b0
Lncvrt <lncvrtreal@gmail.com>
2024-07-24 06:37:25 -0700

    HUD Selection

--- gptware.lua
+++ gptware.lua
@@ -5,6 +5,14 @@ local player = game.Players.LocalPlayer
 local isJumpFlying = false
 local jumpFlyStartHeight = 0.0;
 
+local hud
+local hudframe1
+local hudframe2
+local hudicorner1
+local hudicorner2
+local hudtextlabel1
+local hudtextlabel2
+
 Rayfield:Notify({
    Title = "GPTWare Executed!",
    Content = "GPTWare has been Successfully executed!",
@@ -146,7 +154,7 @@ PlayerTab:CreateButton({
     Callback = function()
         player.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
     end,
- })
+})
 
 --client
 
@@ -158,6 +166,43 @@ ClientTab:CreateButton({
         player.Character.Humanoid.JumpPower = 50
         game.Workspace.Gravity = 196.2
         isJumpFlying = false
+        destroyHUDS()
+    end,
+})
+
+ClientTab:CreateDropdown({
+    Name = "HUD",
+    Options = {"GPTWare", "Sigma Jello", "LiquidBounce", "None"},
+    CurrentOption = "GPTWare",
+    MultipleOptions = false,
+    Flag = "HUDSelectionDropdown",
+    Callback = function(Option)
+        local optionFound = false
+        for _, value in ipairs(Option) do
+            if value == "GPTWare" then
+                optionFound = true
+                destroyHUDS()
+                useGPTWareHud()
+                break
+            elseif value == "Sigma Jello" then
+                optionFound = true
+                destroyHUDS()
+                useSigmaJelloHud()
+                break
+            elseif value == "LiquidBounce" then
+                optionFound = true
+                destroyHUDS()
+                useLiquidBounceHUD()
+                break
+            elseif value == "None" then
+                optionFound = true
+                destroyHUDS()
+                break
+            end
+        end
+        if not optionFound then
+            destroyHUDS()
+        end
     end,
 })
 
@@ -177,4 +222,142 @@ function checkJumpFlyingStatus()
 
         wait(0.01)
     end
+end
+
+--huds
+
+function destroyHUDS()
+    if hud and hud.Parent then hud:Destroy() end
+    if hudframe1 and hudframe1.Parent then hudframe1:Destroy() end
+    if hudframe2 and hudframe2.Parent then hudframe2:Destroy() end
+    if hudicorner1 and hudicorner1.Parent then hudicorner1:Destroy() end
+    if hudicorner2 and hudicorner2.Parent then hudicorner2:Destroy() end
+    if hudtextlabel1 and hudtextlabel1.Parent then hudtextlabel1:Destroy() end
+    if hudtextlabel2 and hudtextlabel2.Parent then hudtextlabel2:Destroy() end
+end
+
+function useGPTWareHud()
+    hud = Instance.new("ScreenGui")
+    hudframe1 = Instance.new("Frame")
+    hudframe2 = Instance.new("Frame")
+    hudicorner1 = Instance.new("UICorner")
+    hudicorner2 = Instance.new("UICorner")
+    hudtextlabel1 = Instance.new("TextLabel")
+    
+    hud.Name = "GPTWare HUD"
+    hud.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    hud.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    
+    hudframe1.Parent = hud
+    hudframe1.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+    hudframe1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudframe1.BorderSizePixel = 0
+    hudframe1.Position = UDim2.new(0.0172413792, 0, 0.932330847, 0)
+    hudframe1.Size = UDim2.new(0, 220, 0, 33)
+    
+    hudicorner1.Parent = hudframe1
+    
+    hudtextlabel1.Parent = hudframe1
+    hudtextlabel1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel1.BackgroundTransparency = 1.000
+    hudtextlabel1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudtextlabel1.BorderSizePixel = 0
+    hudtextlabel1.Position = UDim2.new(0, 0, 0.32294035, 0)
+    hudtextlabel1.Size = UDim2.new(0, 213, 0, 20)
+    hudtextlabel1.Font = Enum.Font.SourceSans
+    hudtextlabel1.Text = "GPTWare | Release Build"
+    hudtextlabel1.TextColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel1.TextScaled = true
+    hudtextlabel1.TextSize = 30.000
+    hudtextlabel1.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel1.TextWrapped = true
+    
+    hudframe2.Parent = hudframe1
+    hudframe2.BackgroundColor3 = Color3.fromRGB(134, 26, 240)
+    hudframe2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudframe2.BorderSizePixel = 0
+    hudframe2.Position = UDim2.new(0.0270000007, 0, 0.200000003, 0)
+    hudframe2.Size = UDim2.new(0, 207, 0, 5)
+    
+    hudicorner2.Parent = hudframe2
+end
+
+function useSigmaJelloHud()
+    hud = Instance.new("ScreenGui")
+    hudframe1 = Instance.new("Frame")
+    hudtextlabel1 = Instance.new("TextLabel")
+    hudtextlabel2 = Instance.new("TextLabel")
+
+    hud.Name = "Sigma Jello HUD"
+    hud.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    hud.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+    hudframe1.Parent = hud
+    hudframe1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    hudframe1.BackgroundTransparency = 1.000
+    hudframe1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudframe1.BorderSizePixel = 0
+    hudframe1.Position = UDim2.new(0.0209999997, 0, 0.0209999997, 0)
+    hudframe1.Size = UDim2.new(0, 228, 0, 104)
+
+    hudtextlabel1.Parent = hudframe1
+    hudtextlabel1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel1.BackgroundTransparency = 1.000
+    hudtextlabel1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudtextlabel1.BorderSizePixel = 0
+    hudtextlabel1.Position = UDim2.new(-0.122807018, 0, 0.00961538497, 0)
+    hudtextlabel1.Size = UDim2.new(0, 200, 0, 50)
+    hudtextlabel1.Font = Enum.Font.SourceSansLight
+    hudtextlabel1.Text = "G P T"
+    hudtextlabel1.TextColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel1.TextSize = 69.000
+    hudtextlabel1.TextTransparency = 0.500
+
+    hudtextlabel2.Parent = hudframe1
+    hudtextlabel2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel2.BackgroundTransparency = 1.000
+    hudtextlabel2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudtextlabel2.BorderSizePixel = 0
+    hudtextlabel2.Position = UDim2.new(-0.232456148, 0, 0.490384609, 0)
+    hudtextlabel2.Size = UDim2.new(0, 200, 0, 24)
+    hudtextlabel2.Font = Enum.Font.SourceSansLight
+    hudtextlabel2.Text = "Ware"
+    hudtextlabel2.TextColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel2.TextSize = 37.000
+    hudtextlabel2.TextTransparency = 0.500
+end
+
+function useLiquidBounceHUD()
+    hud = Instance.new("ScreenGui")
+    hudtextlabel1 = Instance.new("TextLabel")
+    hudtextlabel2 = Instance.new("TextLabel")
+
+    hud.Name = "LiquidBounce HUD"
+    hud.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    hud.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+    hudtextlabel1.Parent = hud
+    hudtextlabel1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel1.BackgroundTransparency = 1.000
+    hudtextlabel1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudtextlabel1.BorderSizePixel = 0
+    hudtextlabel1.Position = UDim2.new(0.0164203607, 0, 0.012531328, 0)
+    hudtextlabel1.Size = UDim2.new(0, 200, 0, 57)
+    hudtextlabel1.Font = Enum.Font.SourceSansBold
+    hudtextlabel1.Text = "GPTBounce"
+    hudtextlabel1.TextColor3 = Color3.fromRGB(0, 0, 0)
+    hudtextlabel1.TextSize = 56.000
+    hudtextlabel1.TextTransparency = 0.200
+
+    hudtextlabel2.Parent = hud
+    hudtextlabel2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    hudtextlabel2.BackgroundTransparency = 1.000
+    hudtextlabel2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    hudtextlabel2.BorderSizePixel = 0
+    hudtextlabel2.Position = UDim2.new(0.0106732352, 0, -9.56064472e-09, 0)
+    hudtextlabel2.Size = UDim2.new(0, 207, 0, 68)
+    hudtextlabel2.Font = Enum.Font.SourceSansBold
+    hudtextlabel2.Text = "GPTBounce"
+    hudtextlabel2.TextColor3 = Color3.fromRGB(8, 116, 255)
+    hudtextlabel2.TextSize = 56.000
 end
